777a48de9155ebb04e1a7d09ceff6b4a7b77d106
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2026-05-27 03:40:29 -0400

    Added more customizable parameters

--- Quagmire_Alternating_Alphabets/Quagmire_Alternating_Alphabets.py
+++ Quagmire_Alternating_Alphabets/Quagmire_Alternating_Alphabets.py
@@ -1,18 +1,26 @@
 # SPDX-FileCopyrightText: 2026 DJDevon3
 # SPDX-License-Identifier: MIT
 # Coded for Python 3.10.5
-"""Quagmire Alternating Alphabets 2026-05-26"""
+"""Quagmire Alternating Alphabets 2026-05-27"""
 
 import string
 STD = string.ascii_uppercase
 
-# Script Configuration Options
+"""
+Script Configuration Options
+You can make a standard Caesar matrix by setting alternating to false 
+and number of alphabets to 1
+"""
 ciphertext_mode = "K4"
+alternating_direction = "TRUE" #TRUE or FALSE
+number_of_alphabets = "4" #1 to 4 optional
+#Maximum of 4 keyworded alphabets
 keyword0 = "ABC"
 keyword1 = "ABC"
 keyword2 = "ABC"
 keyword3 = "ABC"
 
+# For adding custom variations that can be switched to with ciphertext mode switch
 if (ciphertext_mode == "CUSTOM"):
     ciphertext = ("EMUFPHZLRFAXYUSDJKZLDKRNSHGNFIVJYQTQUXQBQVYUVLLTREVJYQTMKYRDMFD")
 if (ciphertext_mode == "K4_SNAKE"):
@@ -92,26 +100,51 @@ def print_matrix(matrix):
 
 def save_matrix(matrix, filename):
     """
-    Double-spaced file save
+    Header in text file save with process used
+    This makes backtracking, file searching, or sharing results easier
     """
     with open(filename, "w", encoding="utf-8") as f:
         f.write(f"MULTIPLE ALTERNATING ALPHABETS MATRIX\n")
-        f.write(f"Alphabets: {alphabets}\n")
-        f.write(f"Keywords:  {rkey0}-{rkey1}-{rkey2}-{rkey3}\n")
+        f.write(f"Mode:  {ciphertext_mode}\n")
+        f.write(f"Alternating:  {alternating_direction}\n")
+        f.write(f"Alphabets:{number_of_alphabets} {alphabets}\n")
+        if (number_of_alphabets == "1"):    
+            f.write(f"Keywords:  {rkey0}\n")
+        if (number_of_alphabets == "2"):
+            f.write(f"Keywords:  {rkey0}-{rkey1}\n")
+        if (number_of_alphabets == "3"):
+            f.write(f"Keywords:  {rkey0}-{rkey1}-{rkey2}\n")
+        if (number_of_alphabets == "4"):
+            f.write(f"Keywords:  {rkey0}-{rkey1}-{rkey2}-{rkey3}\n")
         f.write(f"Ciphertext: \n{ciphertext}\n")
         f.write("------------------ Matrix 0 ---------------------------\n")
         for row in matrix:
             f.write(" ".join(row) + "\n")
     print(f"\n\nResults saved to: {filename}")
 
-# Reverse scheme is identical regardless of alternating start. 
-# The column alphabet direction simply reverses, it's 100% recriprocal. 
-# No alternating parameter to switch them is needed. 
-A0 = keyword_alphabet(keyword0)
-A1 = keyword_alphabet(keyword1, reverse=True)
-A2 = keyword_alphabet(keyword2)
-A3 = keyword_alphabet(keyword3, reverse=True)
-alphabets = [A0, A1, A2, A3]
+# Reverse scheme is identical regardless of alternating start with keyword0 or keyword1. 
+# The column alphabet direction reverses and displays the matrix upside down, it's 100% recriprocal. 
+# No alternating parameter to switch them is needed.
+if (alternating_direction):
+    A0 = keyword_alphabet(keyword0)
+    A1 = keyword_alphabet(keyword1, reverse=True)
+    A2 = keyword_alphabet(keyword2)
+    A3 = keyword_alphabet(keyword3, reverse=True)
+if not (alternating_direction):
+    A0 = keyword_alphabet(keyword0)
+    A1 = keyword_alphabet(keyword1)
+    A2 = keyword_alphabet(keyword2)
+    A3 = keyword_alphabet(keyword3)    
+    
+# Current hardcoded limit is 4    
+if (number_of_alphabets == "1"):    
+    alphabets = [A0]
+if (number_of_alphabets == "2"):
+    alphabets = [A0, A1]
+if (number_of_alphabets == "3"):
+    alphabets = [A0, A1, A2]
+if (number_of_alphabets == "4"):
+    alphabets = [A0, A1, A2, A3]
 
 matrix = build_columnar_hybrid_matrix(ciphertext, alphabets)
 
@@ -122,11 +155,25 @@ print("\nMATRIX:")
 print_matrix(matrix)
 
 # Save Results to file
-rkey0 = reverse_keyword(keyword0)
-rkey1 = reverse_keyword(keyword1, reverse=True)
-rkey2 = reverse_keyword(keyword2)
-rkey3 = reverse_keyword(keyword3, reverse=True)
- 
-filename = f"Quagmire Alternating Alphabets Results\{rkey0}-{rkey1}-{rkey2}-{rkey3}_{first_ten}.txt"
+if (alternating_direction):
+    rkey0 = reverse_keyword(keyword0)
+    rkey1 = reverse_keyword(keyword1, reverse=True)
+    rkey2 = reverse_keyword(keyword2)
+    rkey3 = reverse_keyword(keyword3, reverse=True)
+if not (alternating_direction):
+    rkey0 = reverse_keyword(keyword0)
+    rkey1 = reverse_keyword(keyword1)
+    rkey2 = reverse_keyword(keyword2)
+    rkey3 = reverse_keyword(keyword3)
+
+if (number_of_alphabets == "1"):    
+    filename = f"Quagmire Alternating Alphabets Results\{ciphertext_mode}_({rkey0})_{first_ten}.txt"
+if (number_of_alphabets == "2"):
+    filename = f"Quagmire Alternating Alphabets Results\{ciphertext_mode}_({rkey0}-{rkey1})_{first_ten}.txt"
+if (number_of_alphabets == "3"):
+    filename = f"Quagmire Alternating Alphabets Results\{ciphertext_mode}_({rkey0}-{rkey1}-{rkey2})_{first_ten}.txt"
+if (number_of_alphabets == "4"):
+    filename = f"Quagmire Alternating Alphabets Results\{ciphertext_mode}_({rkey0}-{rkey1}-{rkey2}-{rkey3})_{first_ten}.txt"
+    
 save_matrix(matrix, filename)
 
