b0e7e44373f090dd6adb8e864b785719975a7056
DJDevon3 <49322231+DJDevon3@users.noreply.github.com>
2026-06-02 12:25:39 -0400

    many changes
    improved file save with automatic directory creation for results, added optional scytale & skip decryption as post-process after matrix result. Streamlined code update.

--- Quagmire_III_IV/Quagmire_III_IV.py
+++ Quagmire_III_IV/Quagmire_III_IV.py
@@ -2,29 +2,38 @@
 # SPDX-License-Identifier: MIT
 # Coded for Python 3.10.5
 """
-ACA Quagmire III & IV Encrypt & Decrypt 2026-05-29
+ACA Quagmire III & IV Encrypt & Decrypt 2026-06-02
 """
 
+import os
 import string
+import math
 from pathlib import Path
 STD = string.ascii_uppercase
 
 """
 =========================================================
 CONFIGURATION PARAMETERS
-=========================================================
 top keyworded alphabet is row keyword in mode 3
 top keyword only used in mode 4
 """
+QUAGMIRE_MODE = "4" # Valid modes: 3 or 4
 cryptography_mode = "DECRYPT" # Valid modes: ENCRYPT or DECRYPT
-QUAGMIRE_MODE = "3" # Valid modes: 3 or 4
 row_keyword = "KRYPTOS"
 vertical_keyword = "PALIMPSEST"
-top_keyword = "ABC" # Only used with Quagmire 4
+top_keyword = "KRYPTOS" # Only used with Quagmire 4
 ciphertext_mode = "K1"
 
-# For adding custom variations that can be switched to with ciphertext mode switch
-# Either single or double spaced works
+# Optional Additional Scytale Post-Processing
+enable_scytale = False
+scytale_rod_sizes = list(range(1, 15))
+
+# Optional Additional Skip Transposition Post-Processing
+enable_skip = False
+skip_sizes = list(range(1, 15))
+ 
+if (ciphertext_mode == "CUSTOM"):
+    ciphertext = "UNZLSELLXVHVIFYHBJHVWCIWXRZXYXUFYJPQVIUUUSLTWWZLWQITW"
 if (ciphertext_mode == "DEMONSTRATION"):
     ciphertext = "W L Y N P A E K A R B A B P N A E Z K X T E E C Y B K A L M L R D M S R S F D B R W Q B S H H F C U W K I K A F K R B O D K I S F Z L O J U T V X K S B U G W C Q B D"
     cryptography_mode = "DECRYPT"
@@ -40,22 +49,23 @@ if (ciphertext_mode == "K3"):
     ciphertext = "ENDYAHROHNLSRHEOCPTEOIBIDYSHNAIACHTNREYULDSLLSLLNOHSNOSMRWXMNETPRNGATIHNRARPESLNNELEBLPIIACAEWMTWNDITEENRAHCTENEUDRETNHAEOETFOLSEDTIWENHAEIOYTEYQHEENCTAYCREIFTBRSPAMHHEWENATAMATEGYEERLBTEEFOASFIOTUETUAEOTOARMAEERTNRTIBSEDDNIAAHTTMSTEWPIEROAGRIEWFEBAECTDDHILCEIHSITEGOEAOSDDRYDLORITRKLMLEHAGTDHARDPNEOHMGFMFEUHEECDMRIPFEIMEHNLSSTTRTVDOHW"
 if (ciphertext_mode == "K4"):
     ciphertext = "OBKRUOXOGHULBSOLIFBBWFLRVQQPRNGKSSOTWTQSJQSSEKZZWATJKLUDIAWINFBNYPVTTMZFPKWGDKZXTJCDIGKUHUAUEKCAR"
+if (ciphertext_mode == "K4_SNAKE"):
+    ciphertext = "O B K R O S S K G N R P Q Q V R L F W B B F I L O S B L U H G O X O U T W T Q S J Q S S E K Z Z W A T J K L U D I A W I N F B N Y P R A C K E U A U H U K G I D C J T X Z K D G W K P F Z M T T V"
+if (ciphertext_mode == "K4_CRIB"):
+    ciphertext = "x x x x x x x x x x x x x x x x x x x x x E A S T N O R T H E A S T x x x x x x x x x x x x x x x x x x x x x x x x x x x x x B E R L I N C L O C K x x x x x x x x x x x x x x x x x x x x x x x"
 if (ciphertext_mode == "K1_ENCRYPT"):
     ciphertext = "BETWEENSUBTLESHADINGANDTHEABSENCEOFLIGHTLIESTHENUANCEOFIQLUSION"
 if (ciphertext_mode == "K2_ENCRYPT"):
     ciphertext = "I T W A S T O T A L L Y I N V I S I B L E H O W S T H A T P O S S I B L E T H E Y U S E D T H E E A R T H S M A G N E T I C F I E L D X T H E I N F O R M A T I O N W A S G A T H E R E D A N D T R A N S M I T T E D U N D E R G R U U N D T O A N U N K N O W N L O C A T I O N X D O E S L A N G L E Y K N O W A B O U T T H I S T H E Y S H O U L D I T S B U R I E D O U T T H E R E S O M E W H E R E X W H O K N O W S T H E E X A C T L O C A T I O N O N L Y W W T H I S W A S H I S L A S T M E S S A G E X T H I R T Y E I G H T D E G R E E S F I F T Y S E V E N M I N U T E S S I X P O I N T F I V E S E C O N D S N O R T H S E V E N T Y S E V E N D E G R E E S E I G H T M I N U T E S F O R T Y F O U R S E C O N D S W E S T I D B Y R O W S"
     
-    
 # =========================================================
 # Utility Functions
 # =========================================================
-
 def clean_text(text):
     return ''.join(
         c for c in text.upper()
         if c in STD
     )
-
 
 def keyed_alphabet(keyword):
     keyword = clean_text(keyword)
@@ -67,16 +77,13 @@ def keyed_alphabet(keyword):
             result.append(ch)
     return ''.join(result)
 
-
 def rotate_alphabet(alphabet, start_char):
     idx = alphabet.index(start_char)
     return alphabet[idx:] + alphabet[:idx]
 
-
 def double_space(text):
     text = clean_text(text)
     return " ".join(text)
-
 
 def repeat_keyword(keyword, length):
     keyword = clean_text(keyword)
@@ -86,11 +93,18 @@ def repeat_keyword(keyword, length):
     repeated = repeated[:length]
     return repeated
 
+def output(text, file_handle=None):
+    """
+    Print to console and optionally save to file.
+    """
+    print(text)
+
+    if file_handle:
+        file_handle.write(str(text) + "\n")
 
 # =========================================================
 # Build Tableau
 # =========================================================
-
 def build_tableau(row_alphabet, vertical_keyword):
     vertical_alpha = keyed_alphabet(
         vertical_keyword
@@ -104,58 +118,29 @@ def build_tableau(row_alphabet, vertical_keyword):
         matrix.append(row)
     return matrix
 
-
 # =========================================================
 # Format Output
 # =========================================================
-
-def tableau_to_string(
-    mode,
-    top_alphabet,
-    top_keyword,
-    row_alphabet,
-    row_keyword,
-    vertical_keyword,
-    matrix,
-    ciphertext,
-    plaintext
-):
-
+def tableau_to_string(mode, top_alphabet, top_keyword, row_alphabet, row_keyword, vertical_keyword, matrix, ciphertext, plaintext):
     lines = []
     ciphertext_clean = clean_text(ciphertext)
     repeated_keyword = repeat_keyword(vertical_keyword,len(ciphertext_clean))
-
-    # =====================================================
+    
     # Quagmire III
-    # =====================================================
-
     if mode == "3":
         lines.append("    Quagmire III Table")
-        lines.append(f"    Keyworded Alphabet: {row_alphabet}")
-        lines.append(f"    Vertical Keyword: {vertical_keyword}")
         lines.append("    -  " + " ".join(row_alphabet))
-
-    # =====================================================
+        
     # Quagmire IV
-    # =====================================================
-
     elif mode == "4":
         lines.append("    Quagmire IV Table")
-        lines.append(f"    Top Alphabet: {top_alphabet} ({top_keyword})")
-        lines.append(f"    Row Alphabet: {row_alphabet} ({row_keyword})")
-        lines.append(f"    Vertical Keyword: {vertical_keyword}")
         lines.append("    -  " + " ".join(top_alphabet))
     else:
         raise ValueError("INVALID QUAGMIRE MODE MUST BE 3 OR 4")
-
-    # =====================================================
-    # Matrix Rows
-    # =====================================================
-
     for i, row in enumerate(matrix):
         row_string = " ".join(row)
         lines.append(f"    {i+1:02d} {row_string}")
-
+        
     lines.append("\nKeyword")
     lines.append(double_space(repeated_keyword))
     if (cryptography_mode == "DECRYPT"):
@@ -170,14 +155,9 @@ def tableau_to_string(
         lines.append(double_space(plaintext))
     return "\n".join(lines)
 
-
 # =========================================================
 # Save Output
 # =========================================================
-
-def save_output(filename, text):
-    Path(filename).write_text(text,encoding="utf-8")
-
 def decrypt_quagmire(ciphertext,top_alphabet,row_alphabet,vertical_keyword):
     ciphertext = clean_text(ciphertext)
     vertical_keyword = clean_text(vertical_keyword)
@@ -208,13 +188,9 @@ def encrypt_quagmire(plaintext,top_alphabet,row_alphabet,vertical_keyword):
     for i, pt_char in enumerate(plaintext):
         indicator_char = repeated_keyword[i]
         
-        # Build tableau row
+        # Build tableau
         row = rotate_alphabet(row_alphabet,indicator_char)
-
-        # Find plaintext letter position
         col = top_alphabet.index(pt_char)
-
-        # Ciphertext from tableau row
         ct_char = row[col]
         ciphertext.append(ct_char)
     return ''.join(ciphertext)
@@ -229,14 +205,73 @@ else:
     raise ValueError(
         "INVALID QUAGMIRE MODE MUST BE 3 OR 4"
     )
+
+# ==========================================
+# SCYTALE FUNCTIONS
+# ==========================================
+def scytale_decrypt(text, rod_size):
+    """
+    Standard scytale decryption
+    """
+    length = len(text)
+    cols = math.ceil(length / rod_size)
+    grid = [['' for _ in range(cols)] for _ in range(rod_size)]
+    idx = 0
+    for r in range(rod_size):
+        for c in range(cols):
+
+            if idx < length:
+                grid[r][c] = text[idx]
+                idx += 1
+    plaintext = ""
+    for c in range(cols):
+        for r in range(rod_size):
+
+            if grid[r][c]:
+                plaintext += grid[r][c]
+    return plaintext
+
+def get_scytale_results(text, rod_sizes):
+    lines = []
+    lines.append("\n=============== Scytale Results ===============")
+    for rod in rod_sizes:
+        result = scytale_decrypt(text, rod)
+        spaced = " ".join(result)
+        lines.append(f"\nSkip:{rod:02} | {spaced}")
+    return "\n".join(lines)
+ 
+# ==========================================
+# SKIP CIPHER FUNCTIONS
+# ========================================== 
+def skip_transposition(text, skip):
+    """
+    Skip transposition
+    """
+    step = skip + 1
+    result = []
+    for start in range(step):
+        result.append(text[start::step])
+    return ''.join(result)
     
-# build matrix and decrypt
+def get_skip_results(text, skip_sizes):
+    lines = []
+    lines.append("\n=============== Skip Transposition Results ===============")
+
+    for skip in skip_sizes:
+        result = skip_transposition(text, skip)
+        spaced = " ".join(result)
+        lines.append(f"\nSkip:{skip:02} | {spaced}")
+    return "\n".join(lines)
+    
+# ==========================================
+# RUN PROGRAM
+# ========================================== 
 matrix = build_tableau(row_alphabet,vertical_keyword)
 if (cryptography_mode=="DECRYPT"):
     plaintext = decrypt_quagmire(ciphertext,top_alphabet,row_alphabet,vertical_keyword)
 if (cryptography_mode=="ENCRYPT"):
     plaintext = encrypt_quagmire(ciphertext,top_alphabet,row_alphabet,vertical_keyword)
-    
+
 # console print format
 tableau_output = tableau_to_string(
     mode=QUAGMIRE_MODE,
@@ -249,11 +284,28 @@ tableau_output = tableau_to_string(
     ciphertext=ciphertext,
     plaintext=plaintext
 )
-print(f"\n{tableau_output}\n")
 
 # save to txt file
-# Quagmire Results folder does not automatically create itself
 first_ten = ciphertext[0:10]
-output_filename = (f"Quagmire Results\{cryptography_mode}_Q{QUAGMIRE_MODE.replace(' ', '_')}_({row_keyword}-{vertical_keyword})_{first_ten}.txt")
-save_output(output_filename,tableau_output)
-print(f"Saved to:{output_filename}")
+output_dir = "Quagmire III IV Results"
+os.makedirs(output_dir, exist_ok=True)
+filename = os.path.join(output_dir,f"{cryptography_mode}_Q{QUAGMIRE_MODE.replace(' ', '_')}_({row_keyword}-{vertical_keyword})_{first_ten}.txt")
+
+with open(filename, "w", encoding="utf-8") as f:
+    output("=" * 45, f)
+    output(f"Method: {cryptography_mode}", f)
+    output(f"Top Alphabet: {top_alphabet}", f)
+    output(f"Row Alphabet: {row_alphabet}", f)
+    output(f"Vertical Keyword: {vertical_keyword}", f)
+    output(f"Custom Mode: {QUAGMIRE_MODE.replace(' ', '_')}", f)
+    output(f"Ciphertext: {ciphertext}", f)
+    output(f"Ciphertext Length: {len(ciphertext)}\n", f)
+    output(f"{tableau_output}", f)
+    if enable_scytale:
+        scytale_output = get_scytale_results(plaintext, scytale_rod_sizes)
+        output(scytale_output, f)
+    if enable_skip:
+        skip_output = get_skip_results(plaintext, skip_sizes)
+        output(skip_output, f)
+    
+print(f"\nResults saved to: {filename}")
